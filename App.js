baf9cd873ea72f83f1f5b995547df279cd02cffd
ivan-the-best <51818187+ivan-the-best@users.noreply.github.com>
2020-05-15 05:11:41 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,43 +1,121 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from 'react';
+import { Button, View, Text, TextInput } from 'react-native';
+import { Container, Header, Content, Item } from 'native-base';
 
-import { matrixScreen } from './screens/matrixScreen';
-
-
-
-function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>What should we do?</Text>
-            <Button
-                title="Matrix"
-                onPress={() => navigation.navigate('Matrix')}
-            />
-        </View>
-    );
+function updateMatrix(_m, _n) {
+    if (_n == null && _m !== null)
+        this.setState({ m: _m });
+    if (_m == null && _n !== null)
+        this.setState({ n: _n });
 }
 
-const Stack = createStackNavigator();
 
-function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Matrix" component={matrixScreen} /> 
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+
+
+class Matrix extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { m: props.m, n: props.n };
+        updateMatrix = updateMatrix.bind(this)
+    }
+
+    componentDidMount() {
+        //this.setState({m: 4, n:5});
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        const matrix = []; 
+        for (let i = 0; i < this.state.m; i++) {
+            for (let j = 0; j < this.state.n; j++) {
+                matrix.push(
+                    <TextInput
+                        style={{ width: '15%', marginLeft: '15%', paddingLeft: '5%', borderWidth: 1 }}
+                        placeholder={i.toString()}
+                        defaultValue=''
+                        key={{ i }.toString(), { j }.toString()}
+                    />
+                )
+            }
+            //matrix.push(
+            //    <View />
+            //)
+        }
+        return (
+            <View>
+                {matrix}  
+            </View>
+        )
+    }
 }
 
-export default App;
+class MatrixInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            valueM: props.defaultValueM,
+            valueN: props.defaultValueN
+        };
+    }
+    componentDidMount() {
 
-<Stack.Screen
-    name="Home"
-    component={HomeScreen}
-    options={{ title: 'Overview' }}
-/>
+    }
+    componentWillUnmount() {
 
+    }
+    handleChangeM(_value) {
+        this.setState({ valueM: _value });
+        updateMatrix(_value, null);
+    }
+    handleChangeN(_value) {
+        this.setState({ valueN: _value });
+        updateMatrix(null, _value);
+    }
+
+    render() {
+        return (
+            <View >
+                <TextInput
+                    style={{ width: '50%',margin:'7%', backgroundColor: '#D8D8D8' }}
+                    placeholder={this.props.placeholderM}
+                    onChangeText={value => this.handleChangeM(value)}
+                    //defaultValue={this.props.defaultValueM}
+                    value={this.state.valueM}
+                />
+                <TextInput
+                    style={{ width: '50%', margin: '7%', backgroundColor: '#D8D8D8' }}
+                    placeholder={this.props.placeholderN}
+                    onChangeText={value => this.handleChangeN(value)}
+                    //defaultValue={this.props.defaultValueN}
+                    value={this.state.valueN}
+                />
+                
+            </View>
+        )
+    }
+}
+
+export default function App() {
+    return (
+        <Container >
+            <Content>
+                <Item style={{ backgroundColor: '#1a1aff', height: '10%'}}>
+                    <Text />
+                </Item>
+
+                
+
+                <Item>
+                    <MatrixInput defaultValueN="3" defaultValueM="3" />
+                </Item>
+                <Item>
+                    <Matrix m={3} n={3} />
+                </Item>
+            </Content>
+        </Container>
+    )
+}
 
